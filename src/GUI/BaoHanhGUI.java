d7da74e5ab77cd7c70dacfa499eea52c2e52e943
huyhuyhuy112233 <nguyenanhhuy2004@gmail.com>
2024-04-08 14:06:57 +0700

    baohanhgui and jdbc

--- src/GUI/BaoHanhGUI.java
+++ src/GUI/BaoHanhGUI.java
@@ -3,18 +3,19 @@ package GUI;
 import java.awt.*;
 import javax.swing.*;
 
-public class BaoHanhGUI {
-    static JTextField productNameField, warrantyPeriodField,warrantyReasonField,searchField;
+public class BaoHanhGUI extends javax.swing.JPanel {
+    static JTextField productNameField, warrantyPeriodField, warrantyReasonField, searchField;
     static JButton submitButton;
     static JTable table;
 
-    public static JPanel baoHanhGUI() {
-        JPanel mainPanel = new JPanel(new BorderLayout());
+    public BaoHanhGUI() {
+        setLayout(new BorderLayout());
         
+        Font font = new Font(getFont().getName(), Font.PLAIN, 16);
         // Panel for input fields
         JPanel inputPanel = new JPanel(new GridBagLayout());
         inputPanel.setBorder(BorderFactory.createTitledBorder("Thông tin sản phẩm"));
-
+        
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -22,7 +23,7 @@ public class BaoHanhGUI {
 
         JLabel productNameLabel = new JLabel("Tên sản phẩm:");
         inputPanel.add(productNameLabel, gbc);
-
+        productNameLabel.setFont(font);
         gbc.gridx = 1;
         gbc.gridwidth = 2;
         productNameField = new JTextField(20);
@@ -33,7 +34,7 @@ public class BaoHanhGUI {
         gbc.gridwidth = 1;
         JLabel warrantyPeriodLabel = new JLabel("Thời gian bảo hành:");
         inputPanel.add(warrantyPeriodLabel, gbc);
-
+        warrantyPeriodLabel.setFont(font);
         gbc.gridx = 1;
         warrantyPeriodField = new JTextField(20);
         inputPanel.add(warrantyPeriodField, gbc);
@@ -41,43 +42,55 @@ public class BaoHanhGUI {
         gbc.gridx = 0;
         gbc.gridy++;
         JLabel reasonLabel = new JLabel("Lý do bảo hành:");
+        reasonLabel.setFont(font);
         inputPanel.add(reasonLabel, gbc);
 
         gbc.gridx = 1;
         warrantyReasonField = new JTextField(20); // Change from JComboBox to JTextField
         inputPanel.add(warrantyReasonField, gbc);
-        mainPanel.add(inputPanel, BorderLayout.NORTH);
+        add(inputPanel, BorderLayout.NORTH);
+
         // Panel for table
         JPanel tablePanel = new JPanel(new BorderLayout());
         tablePanel.setBorder(BorderFactory.createTitledBorder("Danh sách sản phẩm"));
+       
         table = new JTable();
-        JScrollPane scrollPane = new JScrollPane(table);
+        JScrollPane scrollPane = new JScrollPane(table);      
         // Panel for search input
         JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         searchPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 5));
         JLabel searchLabel = new JLabel("Tìm kiếm:");
+        searchLabel.setFont(font);
         searchPanel.add(searchLabel);
-        searchField = new JTextField(15);
-        searchPanel.add(searchField);
+        searchField = new JTextField(20);
+        searchPanel.add(searchField);       
         tablePanel.add(searchPanel, BorderLayout.NORTH);
         tablePanel.add(scrollPane, BorderLayout.CENTER);
-        
         JButton addButton = new JButton("Thêm");
+        addButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons/add.png")));
         JButton editButton = new JButton("Sửa");
+        editButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons/edit.png")));
         JButton deleteButton = new JButton("Xóa");
+        deleteButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons/delete.png")));
+        
+     // Đặt màu nền cho các nút
+        addButton.setBackground(Color.GREEN); 
+        editButton.setBackground(Color.YELLOW); 
+        deleteButton.setBackground(Color.RED);
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(addButton);
         buttonPanel.add(editButton);
         buttonPanel.add(deleteButton);
         tablePanel.add(buttonPanel, BorderLayout.SOUTH);
-        mainPanel.add(tablePanel, BorderLayout.CENTER);
-        return mainPanel;
+        add(tablePanel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(baoHanhGUI());
+        frame.add(new BaoHanhGUI());
+        frame.setTitle("Thông Tin Bảo hành");
         frame.pack();
         frame.setVisible(true);
     }
--- src/GUI/main.java
+++ src/GUI/main.java
@@ -284,7 +284,7 @@ public class main extends JFrame implements ActionListener, MouseListener {
         pnlBaoHanh = new JPanel();
         pnlCards.add(pnlBaoHanh, "pnlBaoHanh");
         pnlBaoHanh.setLayout(new BorderLayout(0, 0));
-//        pnlBaoHanh.add(new BaoHanhGUI(), BorderLayout.CENTER);
+        pnlBaoHanh.add(new BaoHanhGUI(), BorderLayout.CENTER);
         
         pnlTrangChu = new JPanel();
         pnlCards.add(pnlTrangChu, "pnlTrangChu");
