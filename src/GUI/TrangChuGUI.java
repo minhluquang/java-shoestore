428573b3cf834ecefa18b72d59cc4426201eb7d1
manas1124 <khanhday1124@gmail.com>
2024-05-05 11:03:26 +0700

    anh trang chu

--- src/GUI/TrangChuGUI.java
+++ src/GUI/TrangChuGUI.java
@@ -1,16 +1,32 @@
 package GUI;
 
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TrangChuGUI extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	public String absolutePath = new File("").getAbsolutePath();
 
 	/**
 	 * Create the panel.
 	 */
 	public TrangChuGUI() {
-
+		JPanel pnlHome = new JPanel();
+		pnlHome.setLayout(new GridLayout(1, 0));
+		JLabel lblAnhTrangChu = new JLabel();
+		ImageIcon icon = new ImageIcon(absolutePath + "/src/images/images/trangchu.jpg");
+		Image image = icon.getImage();
+		Image scaledImage = image.getScaledInstance(1280, 720, Image.SCALE_DEFAULT);
+		icon = new ImageIcon(scaledImage);
+		lblAnhTrangChu.setIcon(icon);
+		pnlHome.add(lblAnhTrangChu);
+		add(pnlHome);
 	}
 
 }
